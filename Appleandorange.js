f07a2054048da4d40c6b90d589b228b453aa491b
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-28 20:40:28 +0700

    Update Appleandorange.js

--- Appleandorange.js
+++ Appleandorange.js
@@ -1,4 +1,5 @@
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    //deklarasi apel sebagai array mapping
     const appleRes = apples.map(num => num + a).filter(num => num >= s && num <= t).length;
     
     const orangeRes = oranges.map(num => num + b).filter(num => num >= s && num <= t).length;
